37f4c46d190089efe0645e6b7f90489bfe49dc01
kiyotaka0819 <hiroya741@gmail.com>
2025-08-02 03:55:21 +0900

    CSS調整

--- web_app/app.py
+++ web_app/app.py
@@ -1,5 +1,5 @@
 # FlaskというWebフレームワークをimport
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Flask()アプリケーションのインスタンスを作成
 # __name__は現在のモジュール名を示す特別な変数
@@ -15,11 +15,20 @@ def hello():
     # `render_template`は、HTMLテンプレートをレンダリングする関数
     return render_template('test.html')
 
+# フォームからデータを受け取るルート
+# `methods=['POST']`にすることで、フォームの送信（POSTリクエスト）だけを受け付ける
+@app.route('/register', methods=['POST'])
+def register():
+
+    name = request.form.get('name')
+    email = request.form.get('email')
+    return render_template('ororon.html', name=name, email=email)
+
 # `if __name__ == '__main__':` は、Pythonの標準的な書き方
 # これにより、他のモジュールからインポートされたときには
 # この部分のコードは実行されない
 if __name__ == '__main__':
-    # Webアプリを起動する命令や
+    # Webアプリを起動する命令
     # debug=Trueにしとくと、コードを書き換えたときに
     # 自動的にWebアプリが再起動する
     # これで開発が楽になる
